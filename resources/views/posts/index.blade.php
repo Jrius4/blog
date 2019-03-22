87a641490057fc51c02fa3ae367337beea3eb803
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-03-22 09:59:00 +0300

    working on landing page designs

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 <div class="card p-2">
     <div class="row">
         <div class="col-md-4 col-sm-4">
-            <img class="w-100" src="/storage/cover_images/{{$post->cover_image}}" alt="cover-image"/>
+            <img class="w-100" src="storage/cover_images/{{$post->cover_image}}" alt="cover-image"/>
         </div>
         <div class="col-md-8 col-sm-8">
                 <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
