4a98c66d389dc81b0782e65c52707c69e4c17883
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-03-22 12:16:52 +0300

    added video header

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,35 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid d-flex justify-content-center">
-        <div class="media row m-auto">
-                    <div class="col-md-4 m-2 w-100 media-body">
-                    <h2 class="blog-h">Digital Marketing Skills</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, ipsum neque odio voluptates </p>
-                    <a class="btn btn-outline-danger" href="#">Read More &raquo;</a>
-                    </div>
-                    <div class="col-md-4 m-2 align-self-center ml-auto w-100">
-                    <img class="w-100 h-50" src="{{asset('images/girl404.jpg')}}"/>
-                    </div>
-        </div>
+<div>
 
-        
-</div>
-<section class="col-md-12">
-        <h2 class="blog-h text-center">As ndebitech, this is what we do</h2>
-        huhfdsuhafe8a89
-        <div class="row">
-            <div class="col-sm-3">col</div>
-            <div class="col-sm-3">col</div>
-            <div class="col-sm-3">col</div>
-        </div>
-    </section>
-    <section class="col-md-12">
-        <h2 class="blog-h text-center">As ndeis what we do</h2>
-        <div class="row">
-            <div class="col-sm-3">col</div>
-            <div class="col-sm-3">col</div>
-            <div class="col-sm-3">col</div>
-        </div>
-    </section>
+        <header>
+                <div class="overlay"></div>
+                <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+                  <source src="{{asset('videos/video5.mp4')}}" type="video/mp4">
+                </video>
+                <div class="container h-100">
+                  <div class="d-flex text-center h-100">
+                    <div class="my-auto w-100 text-white">
+                      <h1 class="display-3">Ndebi tech</h1>
+                      <h2>let's talk tek</h2>
+                    </div>
+                  </div>
+                </div>
+              </header>
+
+            <main>
+                <section>
+                        <div>
+                                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                                  <div class="col-md-6 p-4 d-flex flex-column position-static">
+                                    <strong class="d-inline-block mb-2 text-success">Design</strong>
+                                    <h3 class="mb-0">Post title</h3>
+                                    <div class="mb-1 text-muted">Nov 11</div>
+                                    <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#" class="stretched-link">Continue reading</a>
+                                  </div>
+                                  <div class="col-md-6 d-lg-block">
+                                    <svg class="bd-placeholder-img" width="100%" height="auto" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                                  </div>
+                                </div>
+                              </div>
+                </section>
+
+                <section>
+
+                </section>
+
+
+            </main>
 @endsection
